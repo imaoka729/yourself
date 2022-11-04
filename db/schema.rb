789c0b97bd591aede83ce23a1e027d8eb636d5cb
3422 <3422@example.com>
2022-11-04 09:23:31 +0900

    コミット

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2022_10_28_014526) do
     t.date "expiration_date"
     t.date "purchase_date"
     t.integer "quantity"
-ActiveRecord::Schema.define(version: 2022_10_26_020821) do
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "account_id"
