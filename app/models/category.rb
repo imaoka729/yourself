952d55229b2033703a76e1cd2c5e3a841c645b19
3422 <3422@example.com>
2022-11-11 09:27:15 +0900

    11/11

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,17 +2,7 @@ class Category < ApplicationRecord
 
  has_many :foods
 
- validate :error_check
+ validates :category_name, presence: { message: "は必ず入力して下さい" }
+ validates :category_name, uniqueness: { scope: :user_id, message: "はすでに追加されています" }
  
-  def error_check
-   if category_name.blank?
-     errors[:base] << 'カテゴリー名は必ず入力して下さい'
-   else
-     categore = Category.where(user_id: @current_user.id,category_name: category_name)
-     if categore
-       errors[:base] << '同じ名前は一つである必要があります'
-    end
-   end
- end
-
 end
