ff0e3e6ceb1ba24f61f371091e7ac342cc5aa75f
3402 <3402@example.com>
2022-11-15 09:25:53 +0900

    'main'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
 
-    validate :error_check
-    validates :account_id, uniqueness: { case_sensitive: false }
+    #validate :error_check
+    validates :account_id, uniqueness: { case_sensitive: false, message:"は既に使用されています" } 
+    validates :account_id, presence: { message: "は必ず入力して下さい" }
+    validates :account_id, length: {maximum: 100, message: "は100文字以下で入力してください"}
+    validates :passward, presence: { message: "は必ず入力して下さい" }
+    validates :passward, length: {minimum: 4, maximum: 10, message: "は4文字以上、10文字以下で入力してください"}
+    validates :user_name, presence: { message: "は必ず入力して下さい" }
+    validates :user_name, length: {maximum: 100, message: "は100文字以下で入力してください"}
 
-    def error_check
+
+
+    def error_check        #上のvalidatesでチェック中
       if account_id.blank?
         errors[:base] << 'アカウントidは必ず入力して下さい'
       end
