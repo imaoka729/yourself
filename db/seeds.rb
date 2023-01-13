0a56d6adb1a8cc7e4f3bc488b4054cf42eb22f56
3402 <3402@example.com>
2023-01-13 10:10:35 +0900

    'main'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  account_id: 'apple',
+  passward: 'aaaa',
+  user_name: 'りんご',
+  admin: true
+)
+    
