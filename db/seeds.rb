ee3888ffb4d2508e40397a8c4140fd7a3101c114
3418 <3418@example.com>
2023-01-27 09:38:54 +0900

    0127

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#initial_user = User.create!(
-#  account_id: 'apple',
-#  passward: 'aaaa',
-#  user_name: 'りんご',
-#  admin: true
-#)
+User.destroy_all
+Food.destroy_all
+Category.destroy_all
+Saving.destroy_all
+
+initial_user = User.create!(
+  account_id: 'noriyoshi',
+  passward: 'nnnn',
+  user_name: 'のりよし',
+  admin: true
+)
+
+
 
 user1 = User.create!(
-  account_id: 'banana',
-  passward: 'bbbb',
-  user_name: 'ばなな',
+  account_id: 'sasaki',
+  passward: 'ssss',
+  user_name: 'ささき',
   admin: false
 
 )
 
 user1_category = Category.create!(user_id: user1.id, category_name: 'パン類')
 Food.create!(user_id: user1.id, food_name: "クリームパン", category_id: user1_category.id, syoumi_syouhi: "0", expiration_date: "2023-01-15", purchase_date: "2023-01-15", quantity: "3")
+
+Saving.create!(account_id: user1.id, income_pay_date: "2022-12-1", income: "1000000" )
+Saving.create!(account_id: user1.id, income_pay_date: "2023-01-01", pay: "500000" )
+Saving.create!(account_id: user1.id, income_pay_date: "2023-01-08", pay: "400000" )
+
+user2 = User.create!(
+  account_id: 'satoru',
+  passward: 'ssss',
+  user_name: 'さとる',
+  admin: false
+
+)
