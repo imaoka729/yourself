d085f9d9d9cd3f5c4bd9c4200a9dab83bd1997a2
3422 <3422@example.com>
2023-01-27 10:00:39 +0900

    1/27

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,13 @@ user1 = User.create!(
 )
 
 user1_category = Category.create!(user_id: user1.id, category_name: 'パン類')
-Food.create!(user_id: user1.id, food_name: "クリームパン", category_id: user1_category.id, syoumi_syouhi: "0", expiration_date: "2023-01-15", purchase_date: "2023-01-15", quantity: "3")
+user1_category_2 = Category.create!(user_id: user1.id, category_name: '麺類')
+Food.create!(user_id: user1.id, food_name: 'メロンパン', category_id: user1_category.id, syoumi_syouhi: 1, expiration_date: '2023-01-27', purchase_date: '2023-01-26', quantity: 1)
+Food.create!(user_id: user1.id, food_name: 'そば', category_id: user1_category_2.id, syoumi_syouhi: 0, expiration_date: '2023-01-27', purchase_date: '2022-02-02', quantity: 2)
+Food.create!(user_id: user1.id, food_name: 'クリームパン', category_id: user1_category.id, syoumi_syouhi: 1, expiration_date: '2023-01-28', purchase_date: '2023-01-27', quantity: 1)
+Food.create!(user_id: user1.id, food_name: 'パスタ', category_id: user1_category_2.id, syoumi_syouhi: 0, expiration_date: '2023-03-04', purchase_date: '2020-04-26', quantity: 2)
+Food.create!(user_id: user1.id, food_name: 'うどん', category_id: user1_category_2.id, syoumi_syouhi: 0, expiration_date: '2023-06-09', purchase_date: '2022-06-13', quantity: 3)
+Food.create!(user_id: user1.id, food_name: 'パスタ', category_id: user1_category_2.id, syoumi_syouhi: 0, expiration_date: '2026-01-09', purchase_date: '2023-01-18', quantity: 2)
 
 Saving.create!(account_id: user1.id, income_pay_date: "2022-12-1", income: "1000000" )
 Saving.create!(account_id: user1.id, income_pay_date: "2023-01-01", pay: "500000" )
